fd9f87b5310e278406ab6604e1617f33ddd4c0b7
deb1265 <117177026+deb1265@users.noreply.github.com>
2023-12-07 12:56:13 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -48,8 +48,8 @@
 </script>
 
 <svelte:head>
-	<title>Solar API demo</title>
-	<meta name="description" content="Solar API demo" />
+	<title>Patriot Solar Calculator</title>
+	<meta name="description" content="Patriot Solar Calculator" />
 </svelte:head>
 
 <main class="surface on-surface-text body-medium flex flex-col w-screen h-screen">
