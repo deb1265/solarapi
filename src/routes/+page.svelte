df7bc17ae65ad83617591f2758cf46be8adeef0d
deb1265 <117177026+deb1265@users.noreply.github.com>
2023-12-07 16:39:40 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,102 +1,125 @@
-<!-- Your Svelte component file -->
+<!--
+ Copyright 2023 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ -->
+
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import { Loader } from '@googlemaps/js-api-loader';
+	/* global google */
 
-    const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-    const defaultPlace = {
-        address: '1213 Newell Rd, Palo Alto, CA 94303',
-    };
-    let mapElement: HTMLElement;
+	import { Loader } from '@googlemaps/js-api-loader';
+	import { onMount } from 'svelte';
 
-    onMount(async () => {
-        const loader = new Loader({ apiKey: googleMapsApiKey });
-        await loader.load();
+	import SearchBar from './components/SearchBar.svelte';
+	import Sections from './sections/Sections.svelte';
 
-        const geocoder = new google.maps.Geocoder();
-        const geocoderResponse = await geocoder.geocode({ address: defaultPlace.address });
-        const geocoderResult = geocoderResponse.results[0];
-        const location = geocoderResult.geometry.location;
+	const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+	const defaultPlace = {
+		name: 'Rinconada Library',
+		address: '1213 Newell Rd, Palo Alto, CA 94303',
+	};
+	let location: google.maps.LatLng | undefined;
+	const zoom = 19;
 
-        const map = new google.maps.Map(mapElement, {
-            center: location,
-            zoom: 19, // You can adjust the zoom level here
-            tilt: 0,
-            mapTypeId: 'satellite',
-            mapTypeControl: false,
-            fullscreenControl: false,
-            rotateControl: false,
-            streetViewControl: false,
-            zoomControl: false,
-            styles: [{
-                featureType: "poi",
-                elementType: "labels",
-                stylers: [{ visibility: "off" }]
-            }]
-        });
-    });
+	// Initialize app.
+	let mapElement: HTMLElement;
+	let map: google.maps.Map;
+	let geometryLibrary: google.maps.GeometryLibrary;
+	let mapsLibrary: google.maps.MapsLibrary;
+	let placesLibrary: google.maps.PlacesLibrary;
+	onMount(async () => {
+		// Load the Google Maps libraries.
+		const loader = new Loader({ apiKey: googleMapsApiKey });
+		const libraries = {
+			geometry: loader.importLibrary('geometry'),
+			maps: loader.importLibrary('maps'),
+			places: loader.importLibrary('places'),
+		};
+		geometryLibrary = await libraries.geometry;
+		mapsLibrary = await libraries.maps;
+		placesLibrary = await libraries.places;
+
+		// Get the address information for the default location.
+		const geocoder = new google.maps.Geocoder();
+		const geocoderResponse = await geocoder.geocode({ address: defaultPlace.address });
+		const geocoderResult = geocoderResponse.results[0];
+
+		// Initialize the map at the desired location.
+		location = geocoderResult.geometry.location;
+		map = new mapsLibrary.Map(mapElement, {
+			center: location,
+			zoom: zoom,
+			tilt: 0,
+			mapTypeId: 'satellite',
+			mapTypeControl: false,
+			fullscreenControl: false,
+			rotateControl: false,
+			streetViewControl: false,
+			zoomControl: false,
+		});
+	});
 </script>
 
-<!-- HTML structure for full-screen map view -->
-<div style="height: 100vh; width: 100vw;">
-    <div bind:this={mapElement} style="height: 100%; width: 100%;" />
-</div>
-        // Load HD image
-        await loadHDImageOfAddress(defaultPlace.address);
-    });
-
-    // Function to load HD image of a given address
-    async function loadHDImageOfAddress(address: string) {
-        const geocoder = new google.maps.Geocoder();
-        const geocoderResponse = await geocoder.geocode({ address: address });
-        const geocoderResult = geocoderResponse.results[0];
-        const location = geocoderResult.geometry.location;
-
-        const imageUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${location.lat()},${location.lng()}&zoom=15&size=600x300&scale=2&key=${googleMapsApiKey}`;
-        const imageElement = document.getElementById('hdImage');
-        if (imageElement) {
-            imageElement.src = imageUrl;
-        }
-    }
-</script>
-
-<!-- HTML structure -->
+<!-- Top bar -->
 <div class="flex flex-row h-full">
-    <!-- Main map -->
-    <div bind:this={mapElement} class="w-full" />
+	<!-- Main map -->
+	<div bind:this={mapElement} class="w-full" />
 
-    <!-- Side bar -->
-    <aside class="flex-none md:w-96 w-80 p-2 pt-3 overflow-auto">
-        <div class="flex flex-col space-y-2 h-full">
-            {#if placesLibrary && map}
-                <SearchBar bind:location {placesLibrary} {map} initialValue={defaultPlace.name} />
-            {/if}
+	<!-- Side bar -->
+	<aside class="flex-none md:w-96 w-80 p-2 pt-3 overflow-auto">
+		<div class="flex flex-col space-y-2 h-full">
+			{#if placesLibrary && map}
+				<SearchBar bind:location {placesLibrary} {map} initialValue={defaultPlace.name} />
+			{/if}
 
-            {#if location}
-                <Sections {location} {map} {geometryLibrary} {googleMapsApiKey} />
-            {/if}
+			<div class="p-4 surface-variant outline-text rounded-lg space-y-3">
+				<p>
+					<a
+						class="primary-text"
+						href="https://developers.google.com/maps/documentation/solar/overview?hl=en"
+						target="_blank"
+					>
+						Two distinct endpoints of the <b>Solar API</b>
+						<md-icon class="text-sm">open_in_new</md-icon>
+					</a>
+					offer many benefits to solar marketplace websites, solar installers, and solar SaaS designers.
+				</p>
 
-            <div class="grow" />
+				<p>
+					<b>Click on an area below</b>
+					to see what type of information the Solar API can provide.
+				</p>
+			</div>
 
-            <div class="flex flex-col items-center w-full">
-                <md-text-button
-                    href="https://github.com/googlemaps-samples/js-solar-potential"
-                    target="_blank"
-                >
-                    View code on GitHub
-                    <img slot="icon" src="github-mark.svg" alt="GitHub" width="16" height="16" />
-                </md-text-button>
-            </div>
+			{#if location}
+				<Sections {location} {map} {geometryLibrary} {googleMapsApiKey} />
+			{/if}
 
-            <span class="pb-4 text-center outline-text label-small">
-                This is not an officially supported Google product.
-            </span>
-        </div>
-    </aside>
+			<div class="grow" />
+
+			<div class="flex flex-col items-center w-full">
+				<md-text-button
+					href="https://github.com/googlemaps-samples/js-solar-potential"
+					target="_blank"
+				>
+					View code on GitHub
+					<img slot="icon" src="github-mark.svg" alt="GitHub" width="16" height="16" />
+				</md-text-button>
+			</div>
+
+			<span class="pb-4 text-center outline-text label-small">
+				This is not an officially supported Google product.
+			</span>
+		</div>
+	</aside>
 </div>
-
-<!-- Element to display the HD image -->
-<div class="hd-image-container">
-    <img id="hdImage" alt="HD View" />
-</div>
-
