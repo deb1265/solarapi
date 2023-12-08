ed967755ce527664fb7a9d36d644d2cb0704fa35
deb1265 <117177026+deb1265@users.noreply.github.com>
2023-12-08 16:00:32 -0500

    Update 400

--- src/routes/components/SearchBar.svelte
+++ src/routes/components/SearchBar.svelte
@@ -14,52 +14,86 @@
  limitations under the License.
  -->
 <script lang="ts">
-	/* global google */
+  import { onMount } from 'svelte';
+  import type { MdFilledTextField } from '@material/web/textfield/filled-text-field';
 
-	import { onMount } from 'svelte';
-	import type { MdFilledTextField } from '@material/web/textfield/filled-text-field';
+  export let placesLibrary: google.maps.PlacesLibrary;
+  export let map: google.maps.Map;
+  export let initialValue = '';
+  export let zoom = 19;
 
-	export let location: google.maps.LatLng | undefined;
+  let textFieldElement: MdFilledTextField;
+  let marker;
+  let geocoder = new google.maps.Geocoder();
+  let inputElement;
 
-	export let placesLibrary: google.maps.PlacesLibrary;
-	export let map: google.maps.Map;
-	export let initialValue = '';
-	export let zoom = 19;
+  onMount(async () => {
+    await textFieldElement.updateComplete;
+    inputElement = textFieldElement.renderRoot.querySelector('input') as HTMLInputElement;
+    inputElement.addEventListener('keydown', handleEnterPress);
 
-	let textFieldElement: MdFilledTextField;
+    const autocomplete = new placesLibrary.Autocomplete(inputElement, {
+      fields: ['geometry'],
+    });
 
-	onMount(async () => {
-		// https://lit.dev/docs/components/shadow-dom/
-		await textFieldElement.updateComplete;
-		const inputElement = textFieldElement.renderRoot.querySelector('input') as HTMLInputElement;
-		const autocomplete = new placesLibrary.Autocomplete(inputElement, {
-			fields: ['formatted_address', 'geometry', 'name'],
-		});
-		autocomplete.addListener('place_changed', async () => {
-			const place = autocomplete.getPlace();
-			if (!place.geometry || !place.geometry.location) {
-				textFieldElement.value = '';
-				return;
-			}
-			if (place.geometry.viewport) {
-				// map.fitBounds(place.geometry.viewport);
-				map.setCenter(place.geometry.location);
-				map.setZoom(zoom);
-			} else {
-				map.setCenter(place.geometry.location);
-				map.setZoom(zoom);
-			}
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+      if (place.geometry && place.geometry.location) {
+        updateMapAndMarker(place.geometry.location);
+      }
+    });
 
-			location = place.geometry.location;
-			if (place.name) {
-				textFieldElement.value = place.name;
-			} else if (place.formatted_address) {
-				textFieldElement.value = place.formatted_address;
-			}
-		});
-	});
+    marker = new google.maps.Marker({
+      map: map,
+      draggable: true
+    });
+
+    marker.addListener('dragend', () => {
+      const newPosition = marker.getPosition();
+      reverseGeocodeAndUpdateTextField(newPosition);
+    });
+
+    map.addListener('click', (e) => {
+      marker.setPosition(e.latLng);
+      reverseGeocodeAndUpdateTextField(e.latLng);
+    });
+  });
+
+  function handleEnterPress(event) {
+    if (event.key === 'Enter') {
+      // Logic to handle the address input when 'Enter' is pressed
+      console.log('Address entered:', textFieldElement.value);
+      // Add your logic here
+    }
+  }
+
+  function updateMapAndMarker(location) {
+    map.setCenter(location);
+    map.setZoom(zoom);
+    marker.setPosition(location);
+  }
+
+  async function reverseGeocodeAndUpdateTextField(location) {
+    try {
+      const results = await geocoder.geocode({ location: location });
+      if (results.results[0]) {
+        textFieldElement.value = results.results[0].formatted_address;
+      } else {
+        textFieldElement.value = 'No address found';
+      }
+    } catch (e) {
+      textFieldElement.value = 'Geocoding failed';
+    }
+  }
+
+  // Cleanup: Remove event listener
+  onDestroy(() => {
+    if (inputElement) {
+      inputElement.removeEventListener('keydown', handleEnterPress);
+    }
+  });
 </script>
 
 <md-filled-text-field bind:this={textFieldElement} label="Search an address" value={initialValue}>
-	<md-icon slot="leadingicon">search</md-icon>
+  <md-icon slot="leadingicon">search</md-icon>
 </md-filled-text-field>
