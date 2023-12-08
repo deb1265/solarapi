4c95ef118f82457bc6ec6dd40d13ae58cfd703de
deb1265 <117177026+deb1265@users.noreply.github.com>
2023-12-08 12:54:49 -0500

    Update SearchBar.svelte

--- src/routes/components/SearchBar.svelte
+++ src/routes/components/SearchBar.svelte
@@ -15,52 +15,58 @@
  -->
 
 <script lang="ts">
-	/* global google */
+  /* global google */
+  import { onMount } from 'svelte';
+  import type { MdFilledTextField } from '@material/web/textfield/filled-text-field';
 
-	import { onMount } from 'svelte';
-	import type { MdFilledTextField } from '@material/web/textfield/filled-text-field';
+  export let location: google.maps.LatLng | undefined;
+  export let placesLibrary: google.maps.PlacesLibrary;
+  export let map: google.maps.Map;
+  export let initialValue = '';
+  export let zoom = 19;
 
-	export let location: google.maps.LatLng | undefined;
+  let textFieldElement: MdFilledTextField;
 
-	export let placesLibrary: google.maps.PlacesLibrary;
-	export let map: google.maps.Map;
-	export let initialValue = '';
-	export let zoom = 19;
+  onMount(async () => {
+    await textFieldElement.updateComplete;
+    const inputElement = textFieldElement.renderRoot.querySelector('input') as HTMLInputElement;
+    const autocomplete = new placesLibrary.Autocomplete(inputElement, {
+      fields: ['formatted_address', 'geometry', 'name'],
+    });
+    
+    autocomplete.addListener('place_changed', async () => {
+      const place = autocomplete.getPlace();
+      if (!place.geometry || !place.geometry.location) {
+        textFieldElement.value = '';
+        return;
+      }
 
-	let textFieldElement: MdFilledTextField;
+      // Update the map pointer before the place_changed logic is finalized
+      updateMapPointer(place.geometry.location);
 
-	onMount(async () => {
-		// https://lit.dev/docs/components/shadow-dom/
-		await textFieldElement.updateComplete;
-		const inputElement = textFieldElement.renderRoot.querySelector('input') as HTMLInputElement;
-		const autocomplete = new placesLibrary.Autocomplete(inputElement, {
-			fields: ['formatted_address', 'geometry', 'name'],
-		});
-		autocomplete.addListener('place_changed', async () => {
-			const place = autocomplete.getPlace();
-			if (!place.geometry || !place.geometry.location) {
-				textFieldElement.value = '';
-				return;
-			}
-			if (place.geometry.viewport) {
-				// map.fitBounds(place.geometry.viewport);
-				map.setCenter(place.geometry.location);
-				map.setZoom(zoom);
-			} else {
-				map.setCenter(place.geometry.location);
-				map.setZoom(zoom);
-			}
+      // Now handle the place_changed event
+      handlePlaceChanged(place);
+    });
+  });
 
-			location = place.geometry.location;
-			if (place.name) {
-				textFieldElement.value = place.name;
-			} else if (place.formatted_address) {
-				textFieldElement.value = place.formatted_address;
-			}
-		});
-	});
+  function updateMapPointer(location) {
+    map.setCenter(location);
+    map.setZoom(zoom);
+    // Update the pointer/marker here if necessary
+  }
+
+  function handlePlaceChanged(place) {
+    location = place.geometry.location;
+    if (place.name) {
+      textFieldElement.value = place.name;
+    } else if (place.formatted_address) {
+      textFieldElement.value = place.formatted_address;
+    }
+    // Any additional logic to handle after the place has changed
+  }
 </script>
 
 <md-filled-text-field bind:this={textFieldElement} label="Search an address" value={initialValue}>
-	<md-icon slot="leadingicon">search</md-icon>
+  <md-icon slot="leadingicon">search</md-icon>
 </md-filled-text-field>
+
